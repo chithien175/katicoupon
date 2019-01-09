0478688d57af9cc3e22729f8dcb99cd9333e67b4
Thien Pham <chithien175@gmail.com>
2019-01-09 13:16:51 +0700

    update

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -23,6 +23,10 @@
 							<li><a rel="nofollow" href="{{ url('/sitemap.xml') }}">Sơ đồ website</a></li>
 						</ul>
 					</div>
+					<div class="col-lg-12 py-2">
+						<a href="//www.dmca.com/Protection/Status.aspx?ID=30a6cf3d-3566-4545-a1af-40de982a2808" title="DMCA.com Protection Status" class="dmca-badge"> <img src ="https://images.dmca.com/Badges/DMCA_badge_trn_60w.png?ID=30a6cf3d-3566-4545-a1af-40de982a2808"  alt="DMCA.com Protection Status" /></a>  <script src="https://images.dmca.com/Badges/DMCABadgeHelper.min.js"> </script>
+					</div>
+					
 				</div>
 			</div>
 		</div>
--- resources/views/posts/related-posts.blade.php
+++ resources/views/posts/related-posts.blade.php
@@ -1,9 +1,9 @@
 <div class="related-posts">
-    <h2>Tin liên quan</h2>
+    <h2>Xem thêm</h2>
     <ul>
         @foreach($relatedPosts as $related_post)
         <li>
-            <a href="">{{ $related_post->title }}</a>
+            <a href="{{ route('post-detail', [$related_post->slug, $related_post->id]) }}">{{ $related_post->title }}</a>
         </li>
         @endforeach
     </ul>
